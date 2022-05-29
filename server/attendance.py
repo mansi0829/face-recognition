15b7b7701d67511bbaffd774921b24a8d501c2d1
mansi0829 <mansigundre1@gmail.com>
2022-05-29 22:10:18 +0530

    sample images added

--- server/attendance.py
+++ server/attendance.py
@@ -8,10 +8,9 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="",
+  password="mansi0403",
   database="faceRecognition"
 )
-
 
 path= 'images'
 image = []
